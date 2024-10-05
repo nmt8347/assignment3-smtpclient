e17f43a91deefb7bc589d4257ca53bc4517786e3
Neil Thakur <56324725+neil-thakur@users.noreply.github.com>
2024-10-05 16:01:45 -0400

    changed bind to connect

--- smtpClient.py
+++ smtpClient.py
@@ -11,7 +11,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Fill in start
     clientSocket = socket(AF_INET, SOCK_STREAM) #SOCK_STREAM is for TCP socket
-    clientSocket.bind((mailserver, port))
+    clientSocket.connect((mailserver, port))
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
